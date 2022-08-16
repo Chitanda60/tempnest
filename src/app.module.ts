bfaa7a51c166c47c1c946c5b3571335b65cc821e
Chitanda60 <freeyinyutian@gmail.com>
2022-08-16 12:20:08 +0800

    feat: update

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import envConfig from '../config/env';
+
+
+// @Module({
+//   imports: [TypeOrmModule.forRoot(), PostsModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [envConfig.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        entities: [],
+        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT', 3306), // 端口号
+        username: configService.get('DB_USER', 'root'), // 用户名
+        password: configService.get('DB_PASSWORD', 'zx86880326'), // 密码
+        database: configService.get('DB_DATABASE', 'zxsql'), //数据库名
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+      }),
+    }),
+    PostsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
